d2b9ba060854d22cd8f9f31cc2a2a433f6a59245
elasticskyx <sam.mathew@gmail.com>
2019-12-04 00:02:18 +0400

    Dataframe using Pandas

--- samples/dataframe.py
+++ samples/dataframe.py
@@ -1,7 +1,12 @@
 import numpy
 import pandas as pd
-myarray = numpy.array([[1,2,3],[4,5,6]])
+myarray = numpy.array([[1,2,3,9],[4,5,6,11]])
 rownames = ['rover','star']
-colnames = ['venus','mars','saturn']
+colnames = ['venus','mars','saturn','jupiter']
 mydataframe = pd.DataFrame(myarray, index=rownames, columns=colnames)
 print(mydataframe)
+print("\n")
+# print("Method 1:")
+# print("One column:\n%s" %mydataframe['venus'])
+# print("Method 2:")
+print("One column:\n%s" %mydataframe.saturn)
